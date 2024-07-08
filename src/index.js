2cc22b66e2404e5fbbf7b04d7c8463d5832af25c
MclPio <113801014+MclPio@users.noreply.github.com>
2024-07-08 17:02:24 -0400

    Add console commands

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import {Todo} from './todo.js';
+import {commands} from './commands.js';
 
-console.log(new Todo())
+//console.log(new Todo('rake grass', 'go to rake grass in city center', '2024-07-09', 'idk'))
+const user_input = commands().getInput()
+
+// lets try to make the todo object using user input:
+const todo_obj = new Todo(user_input.title, user_input.description, user_input.dueDate, user_input.dueTime, user_input.priority);
+console.log(todo_obj);
--- src/todo.js
+++ src/todo.js
@@ -1,10 +1,18 @@
 class Todo {
-  constructor(title, description, dueDate, priority){
+  /**
+   * Create a new Todo.
+   * @param {string} title - The title of the todo.
+   * @param {string} [description=''] - The description of the todo.
+   * @param {Date} [dueDate=null] - The due date of the todo.
+   * @param {Time} [dueTime=null] - The due time of the todo.
+   * @param {1|2|3|4} [priority=4] - The priority of the todo. Must be 1, 2, 3, or 4.
+   */
+  constructor(title, description='', dueDate=null, dueTime=null, priority=4){
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
-    this.priority = priority;
-    this.checklist = [];
+    this.priority = priority; 
+    // priority should have a choice between 1, 2, 3, 4 type integer?
   }
 }
 
